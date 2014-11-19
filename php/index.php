8f744c53868c12332537311daa815ed815eec78d
d488b <dbasinsk@gmail.com>
2014-11-19 14:05:01 -0800

    11.19

--- php/index.php
+++ php/index.php
@@ -3,22 +3,19 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
     $from = 'From: TangledDemo';
-    $to = 'contact@tangledindesign.com';
+    $to = 'info@postmobapp.com';
     $subject = 'Hello';
     $human = $_POST['human'];
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-    if ($_POST['submit'] && $human == '4') {
+    if ($_POST['submit']) {
         if (mail ($to, $subject, $body, $from)) {
 	    echo '<p>Your message has been sent!</p>';
 	} else {
 	    echo '<p>Something went wrong, go back and try again!</p>';
 	}
-    } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
-
     if ($_POST['submit']) {
         if ($name != '' && $email != '') {
             if ($human == '4') {
@@ -27,8 +24,6 @@
     	    } else {
     	        echo '<p>Something went wrong, go back and try again!</p>';
     	    }
-    	} else if ($_POST['submit'] && $human != '4') {
-    	    echo '<p>You answered the anti-spam question incorrectly!</p>';
     	}
         } else {
             echo '<p>You need to fill in all required fields!!</p>';
