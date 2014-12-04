2ae6c0494f31d63c1f86211a0bd60e4227fcb77b
d488b <dbasinsk@gmail.com>
2014-12-03 17:27:49 -0800

    12.3 fixes

--- php/form.php
+++ php/form.php
@@ -5,13 +5,13 @@
     $from = 'From: www.postmobapp.com';
     $to = 'postmobapp@gmail.com';
     $subject = 'Contact';
-
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 ?>
 
 <?php
 if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) { 
+    if (mail ($to, $subject, $body, $from)) {
+        header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location
         echo '<p>Thank you for your email!</p>';
     } else {
         echo '<p>Oops! An error occurred. Try sending your message again.</p>';
