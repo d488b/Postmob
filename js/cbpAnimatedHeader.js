6e9be0ad24a578844e9547af2bb7feb8c7b4de49
d488b <dbasinsk@gmail.com>
2014-12-04 14:25:36 -0800

    12.4

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 200;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
